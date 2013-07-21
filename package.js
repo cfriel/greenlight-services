0cf1f61c8a40a5f7fe744f4e5b71d6d66bf09d66
Chris Friel <cfriel@gmail.com>
2013-07-21 03:13:08 -0400

    creating services functionality

--- package.js
+++ package.js
@@ -2,9 +2,12 @@ Package.describe({
   summary: "Greenlight services site template"
 });
 
+Npm.depends({mongodb: "1.3.4"});
+
 Package.on_use(function (api, where) {
+    api.use('npm', 'server');
+    api.use('router', ['client', 'server']);
     api.add_files('services.js', 'server');
-    console.log("using greenlight services");
 });
 
 Package.on_test(function (api) {
--- services.js
+++ services.js
@@ -1,4 +1,63 @@
-Meteor.Router.add('/services/:id', function(id){
-    console.log("foo");
-    return 'foo!';
+var mongo = Npm.require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+
+Meteor.Router.add('/services/:database/:collection', function(database,coll){
+
+    var MongoClient = mongo.MongoClient;    
+    
+    var res = Meteor.sync(function(done){
+	MongoClient.connect('mongodb://127.0.0.1:27017/'+database, function(err, db) {
+	    if(err) throw err;
+	    
+	    db.collection(coll)
+		.find({})
+		.limit(10)
+		.toArray(function(err, docs) {
+		    done(err, docs);
+		});
+	});
+    });
+
+    if(res.error)
+    {
+	throw new Meteor.Error(401, res.error.message);
+    }
+    else
+    {
+	console.log(res.result);
+	return JSON.stringify(res.result);
+    }
+});
+    
+Meteor.Router.add('/services/:database/:collection/:id', function(database,coll,id){
+
+    var MongoClient = mongo.MongoClient;    
+    
+    var res = Meteor.sync(function(done){
+	MongoClient.connect('mongodb://127.0.0.1:27017/'+database, function(err, db) {
+	    if(err) throw err;
+	    
+	    db.collection(coll)
+		.find({'_id':new BSON.ObjectID(id)})
+		.limit(1)
+		.toArray(function(err, docs) {
+		    done(err, docs[0]);
+		});
+	});
+    });
+
+    if(res.error)
+    {
+	throw new Meteor.Error(401, res.error.message);
+    }
+    else
+    {
+	console.log(res.result);
+	return JSON.stringify(res.result);
+    }
 });

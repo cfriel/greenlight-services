3b2a7408c846c8960e8ef00edb69f65dc5d9e08a
Chris Friel <cfriel@gmail.com>
2013-07-21 01:14:55 -0400

    debugging package

--- package.js
+++ package.js
@@ -4,8 +4,9 @@ Package.describe({
 
 Package.on_use(function (api, where) {
     api.add_files('services.js', 'server');
+    console.log("using greenlight services");
 });
 
 Package.on_test(function (api) {
-  api.add_files('services_tests.js', 'client');
+    api.add_files('services_tests.js', 'client');
 });
--- services.js
+++ services.js
@@ -1,3 +1,4 @@
 Meteor.Router.add('/services/:id', function(id){
+    console.log("foo");
     return 'foo!';
 });

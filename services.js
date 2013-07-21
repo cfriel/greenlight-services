7d6f5b79268251e3585f50929221775a4b68cf44
Chris Friel <cfriel@gmail.com>
2013-07-21 14:26:56 -0400

    updating services functionality

--- services.js
+++ services.js
@@ -6,7 +6,65 @@ var Server = mongo.Server,
 
 var server = new Server('localhost', 27017, {auto_reconnect: true});
 
-Meteor.Router.add('/services/:database/:collection', function(database,coll){
+Meteor.Router.add('/data', function(database){
+
+    var MongoClient = mongo.MongoClient;    
+    
+    var res = Meteor.sync(function(done){
+	MongoClient.connect('mongodb://127.0.0.1:27017/', function(err, db) {
+	    if(err) throw err;
+	    
+
+	    db.executeDbCommand({'listDatabases':1}, function(err, doc) { 
+		done(err,doc);
+	    });
+	    
+	})
+    });
+
+    if(res.error)
+    {
+	throw new Meteor.Error(401, res.error.message);
+    }
+    else
+    {
+	console.log(res.result);
+	return JSON.stringify(res.result);
+    }
+
+
+});
+
+
+Meteor.Router.add('/data/:database', function(database){
+
+    var MongoClient = mongo.MongoClient;    
+    
+    var res = Meteor.sync(function(done){
+	MongoClient.connect('mongodb://127.0.0.1:27017/'+database, function(err, db) {
+	    if(err) throw err;
+	    
+	    db.collectionNames(function(err, collections){
+		done(err, collections);
+	    });
+	    
+	})
+    });
+
+    if(res.error)
+    {
+	throw new Meteor.Error(401, res.error.message);
+    }
+    else
+    {
+	console.log(res.result);
+	return JSON.stringify(res.result);
+    }
+
+
+});
+
+Meteor.Router.add('/data/:database/:collection', function(database,coll){
 
     var MongoClient = mongo.MongoClient;    
     
@@ -34,7 +92,7 @@ Meteor.Router.add('/services/:database/:collection', function(database,coll){
     }
 });
     
-Meteor.Router.add('/services/:database/:collection/:id', function(database,coll,id){
+Meteor.Router.add('/data/:database/:collection/:id', function(database,coll,id){
 
     var MongoClient = mongo.MongoClient;    
     

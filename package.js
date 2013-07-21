d6e7626ec9cc257810ece2b2b3a66085ae9929b5
Chris Friel <cfriel@gmail.com>
2013-07-21 00:56:51 -0400

    adding package configuration

---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  summary: "Greenlight services site template"
+});
+
+Package.on_use(function (api, where) {
+    api.add_files('services.js', 'client');
+});
+
+Package.on_test(function (api) {
+  api.add_files('services_tests.js', 'client');
+});

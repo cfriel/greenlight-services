1e9f9411834af81a9e6f6c7ce6ccd47e4b0207ad
Chris Friel <cfriel@gmail.com>
2013-08-05 15:29:00 -0400

    moving packages

--- server/services.js
+++ server/services.js
@@ -1,10 +1,16 @@
 var name = "services";
 var version = "1.0";
 
+services = function(){};
+
+services.prototype = new services();
+
+Services = services.prototype;
+
 Meteor.startup(function(){
     
     console.log("loading services package");
     
-    Greenlight.register_template(name, version);
+    Greenlight.register_template(name, version, Services);
     
 });
